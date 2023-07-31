691c5c35a71bda200d46c25fdd5fae2c68b35c42
TanapaPalmer <tanapa.palmer@gmail.com>
2023-07-31 17:23:06 -0400

    bulding a palindrome checker

---
+++ script.js
@@ -0,0 +1,32 @@
+function palindrome(text) {
+    if (text.length <= 1) {
+        return false;
+    }
+
+    for (let p = 0; p < text.length / 2; p++) {
+        if (text[p].toLowerCase() !== text[text.length - 1 - p].toLowerCase()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function process_palindrome(e) {
+    e.preventDefault();
+
+    if (text.value === "") {
+        result.innerText = "Of course it's not a palindrome! You goof! You didn't put any words!!!";
+
+    } else {
+
+        if (palindrome(text.value)) {
+            result.innerText = `${text.value} is a palindrome!`;
+
+        } else {
+            result.innerText = `${text.value} is not a palindrome!`;
+        }
+    }
+    
+    text.value = "";
+}
